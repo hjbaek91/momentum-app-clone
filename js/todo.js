d8b29202016780ce670a3265438a76cc392fc6a2
HyunJun Baek <69138182+hjbaek91@users.noreply.github.com>
2022-10-17 14:14:22 +0900

    Created to-do-list deleting function

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(e) {
+  const li = e.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const btn = document.createElement("button");
+  btn.innerText = "❌";
+  btn.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(btn);
   toDoList.appendChild(li);
 }
 
