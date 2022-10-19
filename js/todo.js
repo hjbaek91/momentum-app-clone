abbe1b15ec9b8e7e73645e118ea452f4b07db819
HyunJun Baek <69138182+hjbaek91@users.noreply.github.com>
2022-10-19 14:54:56 +0900

    Created deleting function for every refresh

--- js/todo.js
+++ js/todo.js
@@ -13,12 +13,15 @@ function saveToDos() {
 function deleteToDo(e) {
   const li = e.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const btn = document.createElement("button");
   btn.innerText = "❌";
   btn.addEventListener("click", deleteToDo);
@@ -31,8 +34,12 @@ function handleToDoSubmit(e) {
   e.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
