b3e19162bb51bb9562fed6afa6b9def8f9245cab
HyunJun Baek <69138182+hjbaek91@users.noreply.github.com>
2022-10-20 21:34:57 +0900

    Added weather API

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,18 @@
+const API_KEY = "08a0ebc33a60d73f21258f8e57f018ba";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      const name = data.name;
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
