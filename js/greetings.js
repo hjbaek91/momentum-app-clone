8fcddd649c297b6b06e8c0b9d4495541228be223
HyunJun Baek <69138182+hjbaek91@users.noreply.github.com>
2022-10-22 22:02:47 +0900

    Updated css and background images

--- js/greetings.js
+++ js/greetings.js
@@ -15,7 +15,7 @@ function onLoginSubmit(e) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `Hello, ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
--- js/quotes.js
+++ js/quotes.js
@@ -1,49 +1,42 @@
 const quotes = [
   {
-    quote: "The way to get started is to quit talking and begin doing.",
+    quote: `"The way to get started is to quit talking and begin doing." - `,
     author: "Walt Disney",
   },
   {
-    quote:
-      "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+    quote: `"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough." - `,
     author: "Oprah Winfrey",
   },
   {
-    quote:
-      "Always remember that you are absolutely unique. Just like everyone else.",
+    quote: `"Always remember that you are absolutely unique. Just like everyone else." - `,
     author: "Margaret Mead",
   },
   {
-    quote:
-      "The future belongs to those who believe in the beauty of their dreams.",
+    quote: `"The future belongs to those who believe in the beauty of their dreams. - "`,
     author: "Eleanor Roosevelt",
   },
   {
-    quote:
-      "The best and most beautiful things in the world cannot be seen or even touched — they must be felt with the heart.",
+    quote: `"The best and most beautiful things in the world cannot be seen or even touched — they must be felt with the heart." - `,
     author: "Helen Keller",
   },
   {
-    quote: "Whoever is happy will make others happy too.",
+    quote: `"Whoever is happy will make others happy too." - `,
     author: "Anne Frank",
   },
   {
-    quote: "Never let the fear of striking out keep you from playing the game.",
+    quote: `"Never let the fear of striking out keep you from playing the game." - `,
     author: "Babe Ruth",
   },
   {
-    quote:
-      "You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.",
+    quote: `"You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose." - `,
     author: "Dr. Seuss",
   },
   {
-    quote:
-      "Keep smiling, because life is a beautiful thing and there's so much to smile about.",
+    quote: `"Keep smiling, because life is a beautiful thing and there's so much to smile about." - `,
     author: "Marilyn Monroe",
   },
   {
-    quote:
-      "The question isn't who is going to let me; it's who is going to stop me.",
+    quote: `"The question isn't who is going to let me; it's who is going to stop me." - `,
     author: "Ayn Rand",
   },
 ];
